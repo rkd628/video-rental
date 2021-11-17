13b3f248a337eab465198d2e6ef0afa473e3d4ba
JP <yurihus@gmail.com>
2021-11-17 15:11:59 +0900

    Apply Builder pattern for Video

--- VRUI.java
+++ VRUI.java
@@ -83,8 +83,10 @@ public class VRUI {
 		customers.add(james) ;
 		customers.add(brown) ;
 
-		Video v1 = new Video("v1", Video.CD, Video.REGULAR, new Date()) ;
-		Video v2 = new Video("v2", Video.DVD, Video.NEW_RELEASE, new Date()) ;
+		Video v1 = new Video.VideoBuilder().setTitle("v1").setVideoType(Video.CD)
+				.setPriceCode(Video.REGULAR).build();
+		Video v2 = new Video.VideoBuilder().setTitle("v2").setVideoType(Video.DVD)
+				.setPriceCode(Video.NEW_RELEASE).build();
 		videos.add(v1) ;
 		videos.add(v2) ;
 
